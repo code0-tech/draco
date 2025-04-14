1b46d57d3fb4382bc2a96b5ec4c2265e20fdea87
Raphael <raphael.goetz.7@gmail.com>
2025-04-14 11:00:22 +0200

    feat: added typed flow for redis

--- draco_rest/src/main.rs
+++ draco_rest/src/main.rs
@@ -508,3 +508,173 @@ async fn main() {
         });
     }
 }
+
+fn mock_flow() {
+    let flow = Flow {
+        flow_id: 6,
+        r#type: "REST".to_string(),
+        data_types: vec![DataType {
+            variant: 1,
+            identifier: "1".to_string(),
+            name: vec![],
+            rules: vec![DataTypeRule {
+                variant: 1,
+                config: Some(tucana::shared::Struct {
+                    fields: HashMap::from([(
+                        "pattern".to_string(),
+                        Value {
+                            kind: Some(tucana::shared::value::Kind::StringValue(
+                                "^[0-9]".to_string(),
+                            )),
+                        },
+                    )]),
+                }),
+            }],
+            input_types: vec![],
+            parent_type_identifier: None,
+            return_type: None,
+        }],
+        input_type: Some(DataType {
+            variant: 3,
+            identifier: "2".to_string(),
+            name: vec![],
+            rules: vec![
+                DataTypeRule {
+                    variant: 5,
+                    config: Some(tucana::shared::Struct {
+                        fields: HashMap::from([
+                            (
+                                "key".to_string(),
+                                Value {
+                                    kind: Some(tucana::shared::value::Kind::StringValue(
+                                        "first".to_string(),
+                                    )),
+                                },
+                            ),
+                            (
+                                "type".to_string(),
+                                Value {
+                                    kind: Some(tucana::shared::value::Kind::StringValue(
+                                        "1".to_string(),
+                                    )),
+                                },
+                            ),
+                        ]),
+                    }),
+                },
+                DataTypeRule {
+                    variant: 5,
+                    config: Some(tucana::shared::Struct {
+                        fields: HashMap::from([
+                            (
+                                "key".to_string(),
+                                Value {
+                                    kind: Some(tucana::shared::value::Kind::StringValue(
+                                        "second".to_string(),
+                                    )),
+                                },
+                            ),
+                            (
+                                "type".to_string(),
+                                Value {
+                                    kind: Some(tucana::shared::value::Kind::StringValue(
+                                        "1".to_string(),
+                                    )),
+                                },
+                            ),
+                        ]),
+                    }),
+                },
+            ],
+            input_types: vec![],
+            parent_type_identifier: None,
+            return_type: None,
+        }),
+        settings: vec![
+            FlowSetting {
+                definition: Some(FlowSettingDefinition {
+                    id: "some_database_id".to_string(),
+                    key: "HTTP_METHOD".to_string(),
+                }),
+                object: Some(tucana::shared::Struct {
+                    fields: HashMap::from([(
+                        String::from("method"),
+                        Value {
+                            kind: Some(tucana::shared::value::Kind::StringValue(
+                                "POST".to_string(),
+                            )),
+                        },
+                    )]),
+                }),
+            },
+            FlowSetting {
+                definition: Some(FlowSettingDefinition {
+                    id: "some_database_id".to_string(),
+                    key: "URL".to_string(),
+                }),
+                object: Some(tucana::shared::Struct {
+                    fields: HashMap::from([(
+                        String::from("url"),
+                        Value {
+                            kind: Some(tucana::shared::value::Kind::StringValue(
+                                "/add".to_string(),
+                            )),
+                        },
+                    )]),
+                }),
+            },
+        ],
+        starting_node: Some(tucana::shared::NodeFunction {
+            definition: Some(NodeFunctionDefinition {
+                function_id: "some_database_id".to_string(),
+                runtime_function_id: "standard::function::math::add".to_string(),
+            }),
+            parameters: vec![
+                NodeParameter {
+                    definition: Some(NodeParameterDefinition {
+                        parameter_id: "some_database_id".to_string(),
+                        runtime_parameter_id: "standard::keys::math::add::first".to_string(),
+                    }),
+                    value: Some(tucana::shared::node_parameter::Value::LiteralValue(Value {
+                        kind: Some(tucana::shared::value::Kind::StructValue(
+                            tucana::shared::Struct {
+                                fields: HashMap::from([(
+                                    String::from("first"),
+                                    Value {
+                                        kind: Some(tucana::shared::value::Kind::StringValue(
+                                            "$first$".to_string(),
+                                        )),
+                                    },
+                                )]),
+                            },
+                        )),
+                    })),
+                },
+                NodeParameter {
+                    definition: Some(NodeParameterDefinition {
+                        parameter_id: "some_database_id".to_string(),
+                        runtime_parameter_id: "standard::keys::math::add::second".to_string(),
+                    }),
+                    value: Some(tucana::shared::node_parameter::Value::LiteralValue(Value {
+                        kind: Some(tucana::shared::value::Kind::StructValue(
+                            tucana::shared::Struct {
+                                fields: HashMap::from([(
+                                    String::from("second"),
+                                    Value {
+                                        kind: Some(tucana::shared::value::Kind::StringValue(
+                                            "$second$".to_string(),
+                                        )),
+                                    },
+                                )]),
+                            },
+                        )),
+                    })),
+                },
+            ],
+            next_node: None,
+        }),
+    };
+
+    let json = serde_json::to_string(&flow).unwrap();
+    println!("{}", json);
+}
